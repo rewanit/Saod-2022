56dcfebeb8eb7c027255162c58d21578b6d30ff2
rewanit <49819859+rewanit@users.noreply.github.com>
2022-02-28 11:33:31 +0300

    LR2 + LR1 edits

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -14,7 +14,7 @@ class Program
         var qSortList = new List<(double, int)>();
         var shakerList = new List<(double, int)>();
         
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < 500; i++)
         {
 
 
@@ -24,11 +24,68 @@ class Program
             shakerList.Add(Test(ShakerSort, array.ToArray()));
 
         }
-
-        Console.WriteLine($"qSort\nMs Min:{qSortList.MinBy(x=>x.Item1).Item1} Max:{qSortList.MaxBy(x => x.Item1).Item1} Avg:{qSortList.Sum(x=>x.Item1)/ (float)qSortList.Count}\nSwap Min:{qSortList.MinBy(x => x.Item2).Item2} Max:{qSortList.MaxBy(x => x.Item2).Item2} Avg:{qSortList.Sum(x => x.Item2) / (float)qSortList.Count}");
+        Console.WriteLine("Случайная последовательность ");
+        Console.WriteLine($"QuickSort \nMs Min:{qSortList.MinBy(x => x.Item1).Item1} Max:{qSortList.MaxBy(x => x.Item1).Item1} Avg:{qSortList.Sum(x => x.Item1) / (float)qSortList.Count}\nSwap Min:{qSortList.MinBy(x => x.Item2).Item2} Max:{qSortList.MaxBy(x => x.Item2).Item2} Avg:{qSortList.Sum(x => x.Item2) / (float)qSortList.Count}");
         Console.WriteLine($"ShakerSort\nMs Min:{shakerList.MinBy(x => x.Item1).Item1} Max:{shakerList.MaxBy(x => x.Item1).Item1} Avg:{shakerList.Sum(x => x.Item1) / (float)shakerList.Count}\nSwap Min:{shakerList.MinBy(x => x.Item2).Item2} Max:{shakerList.MaxBy(x => x.Item2).Item2} Avg:{shakerList.Sum(x => x.Item2) / (float)shakerList.Count}");
+        Console.WriteLine();
+        qSortList = new();
+        shakerList = new();
+        for (int i = 0; i < 500; i++)
+        {
 
 
+            var array = Enumerable.Range(1, 1000);
+
+            var arrayHalf = array.TakeLast(500).OrderBy(x => random.Next()).ToList();
+           var  array2half =array.Take(500).ToList();
+            array2half.AddRange(arrayHalf);
+            array = array2half.ToArray();
+            qSortList.Add(Test(QSort, array.ToArray()));
+            shakerList.Add(Test(ShakerSort, array.ToArray()));
+
+        }
+        Console.WriteLine("Полу случайная последовательность");
+
+        Console.WriteLine($"QuickSort \nMs Min:{qSortList.MinBy(x => x.Item1).Item1} Max:{qSortList.MaxBy(x => x.Item1).Item1} Avg:{qSortList.Sum(x => x.Item1) / (float)qSortList.Count}\nSwap Min:{qSortList.MinBy(x => x.Item2).Item2} Max:{qSortList.MaxBy(x => x.Item2).Item2} Avg:{qSortList.Sum(x => x.Item2) / (float)qSortList.Count}");
+        Console.WriteLine($"ShakerSort\nMs Min:{shakerList.MinBy(x => x.Item1).Item1} Max:{shakerList.MaxBy(x => x.Item1).Item1} Avg:{shakerList.Sum(x => x.Item1) / (float)shakerList.Count}\nSwap Min:{shakerList.MinBy(x => x.Item2).Item2} Max:{shakerList.MaxBy(x => x.Item2).Item2} Avg:{shakerList.Sum(x => x.Item2) / (float)shakerList.Count}");
+        Console.WriteLine();
+        qSortList = new();
+        shakerList = new();
+        for (int i = 0; i < 500; i++)
+        {
+
+
+            var array = Enumerable.Range(1, 1000).Reverse().ToArray();
+
+
+
+            qSortList.Add(Test(QSort, array.ToArray()));
+            shakerList.Add(Test(ShakerSort, array.ToArray()));
+
+        }
+        Console.WriteLine("Обратная последовательность");
+
+        Console.WriteLine($"QuickSort \nMs Min:{qSortList.MinBy(x=>x.Item1).Item1} Max:{qSortList.MaxBy(x => x.Item1).Item1} Avg:{qSortList.Sum(x=>x.Item1)/ (float)qSortList.Count}\nSwap Min:{qSortList.MinBy(x => x.Item2).Item2} Max:{qSortList.MaxBy(x => x.Item2).Item2} Avg:{qSortList.Sum(x => x.Item2) / (float)qSortList.Count}");
+        Console.WriteLine($"ShakerSort\nMs Min:{shakerList.MinBy(x => x.Item1).Item1} Max:{shakerList.MaxBy(x => x.Item1).Item1} Avg:{shakerList.Sum(x => x.Item1) / (float)shakerList.Count}\nSwap Min:{shakerList.MinBy(x => x.Item2).Item2} Max:{shakerList.MaxBy(x => x.Item2).Item2} Avg:{shakerList.Sum(x => x.Item2) / (float)shakerList.Count}");
+        Console.WriteLine();
+
+        qSortList = new();
+        shakerList = new();
+        for (int i = 0; i < 500; i++)
+        {
+
+
+            var array = Enumerable.Range(1, 1000);
+
+            qSortList.Add(Test(QSort, array.ToArray()));
+            shakerList.Add(Test(ShakerSort, array.ToArray()));
+
+        }
+        Console.WriteLine("Отсортированая последовательность");
+
+        Console.WriteLine($"QuickSort \nMs Min:{qSortList.MinBy(x => x.Item1).Item1} Max:{qSortList.MaxBy(x => x.Item1).Item1} Avg:{qSortList.Sum(x => x.Item1) / (float)qSortList.Count}\nSwap Min:{qSortList.MinBy(x => x.Item2).Item2} Max:{qSortList.MaxBy(x => x.Item2).Item2} Avg:{qSortList.Sum(x => x.Item2) / (float)qSortList.Count}");
+        Console.WriteLine($"ShakerSort\nMs Min:{shakerList.MinBy(x => x.Item1).Item1} Max:{shakerList.MaxBy(x => x.Item1).Item1} Avg:{shakerList.Sum(x => x.Item1) / (float)shakerList.Count}\nSwap Min:{shakerList.MinBy(x => x.Item2).Item2} Max:{shakerList.MaxBy(x => x.Item2).Item2} Avg:{shakerList.Sum(x => x.Item2) / (float)shakerList.Count}");
+        Console.WriteLine();
 
 
 
@@ -41,7 +98,10 @@ class Program
         stopwatch.Start();
         Sort(copyArray);
         stopwatch.Stop();
-
+        if (!copyArray.OrderBy(x => x).SequenceEqual(copyArray))
+        {
+            throw new Exception("Не отсортировалось");
+        }
         return (stopwatch.ElapsedMilliseconds,count);
     }
 
@@ -117,9 +177,6 @@ class Program
                 if (arr[left] == arr[right]) return right;
 
                 Swap(ref arr[left], ref arr[right]);
-                
-
-
             }
             else
             {
---
+++ LR2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var random = new Random();
+
+        var linear = new List<double>();
+        var binary = new List<double>();
+        for (int i = 0; i < 500; i++)
+        {
+            var array = Enumerable.Range(1, 1000).ToArray();
+            var toFind = array[random.Next(0, array.Length - 1)];
+            linear.Add(Test(LinearFind, toFind, array));
+            binary.Add(Test(BinarySearch, toFind, array));
+        }
+        Console.WriteLine("Отсортированная последовательность");
+        Console.WriteLine($"Линейный\nMs Min:{linear.MinBy(x => x)} Max:{linear.MaxBy(x => x)} Avg:{linear.Sum(x => x) / (float)linear.Count}\nSwap Min:{linear.MinBy(x => x)} Max:{linear.MaxBy(x => x)} Avg:{linear.Sum(x => x) / (float)linear.Count}");
+        Console.WriteLine($"Бинарный\nMs Min:{binary.MinBy(x => x)} Max:{binary.MaxBy(x => x)} Avg:{binary.Sum(x => x) / (float)binary.Count}\nSwap Min:{binary.MinBy(x => x)} Max:{binary.MaxBy(x => x)} Avg:{binary.Sum(x => x) / (float)binary.Count}");
+        Console.WriteLine();
+        for (int i = 0; i < 500; i++)
+        {
+            var array = Enumerable.Range(1, 1000).OrderBy(x=>random.Next()).ToArray();
+            var toFind = array[random.Next(0, array.Length - 1)];
+            linear.Add(Test(LinearFind, toFind, array));
+        }
+        Console.WriteLine("Случайная последовательность");
+        Console.WriteLine($"Линейный\nMs Min:{linear.MinBy(x => x)} Max:{linear.MaxBy(x => x)} Avg:{linear.Sum(x => x) / (float)linear.Count}\nSwap Min:{linear.MinBy(x => x)} Max:{linear.MaxBy(x => x)} Avg:{linear.Sum(x => x) / (float)linear.Count}");
+
+
+
+    }
+
+
+
+
+
+
+    private static double Test(Func<int,int[],int> Find,int toFind, int[] copyArray)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var finded =Find(toFind,copyArray);
+        stopwatch.Stop();
+        if (finded != toFind)
+        {
+            throw new Exception("Не найдено");
+        }
+        return stopwatch.ElapsedMilliseconds;
+    }
+
+
+    static int LinearFind(int toFind,int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i]==toFind)
+            {
+                return array[i];
+            }
+        }
+        return -1;
+    }
+
+
+    static int BinarySearch( int toFind, int[] array)
+    {
+
+        int minNum = 0;
+        int maxNum = array.Length - 1;
+
+        while (minNum <= maxNum)
+        {
+            int mid = (minNum + maxNum) / 2;
+            if (toFind == array[mid])
+            {
+                return ++mid;
+            }
+            else if (toFind < array[mid])
+            {
+                maxNum = mid - 1;
+            }
+            else
+            {
+                minNum = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
+
+}
